1dfb9bde2fc8d55c87a459fc3e603c8b39ddc2e4
Andrin Bertschi <hi@abertschi.ch>
2025-10-10 22:51:12 +0200

    add group bot

--- scripts/send_group.sh
+++ scripts/send_group.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 #
 # Send to test chat group chat
 #
-phone="120363421146964189"
+phone="120363404262081632"
 isGroup=true
 
 "$SCRIPT_DIR/send.sh" --message "$1" --phone "$phone" --group $isGroup
